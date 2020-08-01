45a946df783bdedb52c3e3e05e7046adc24e3f6c
PriscilaCS <pry_chu@hotmail.com>
2020-08-01 16:36:49 -0300

    Atualizando o arquivo app.vue

--- todo-list/src/App.vue
+++ todo-list/src/App.vue
@@ -6,7 +6,7 @@
         <h2 class="ui inverted header">Glaucia Lemos
           <div class="sub header">Cloud Advocate em JavaScript/Node.js @ Microsoft</div>
         </h2>
-        <h3 class="ui inverted header">Rio de Janeiro, Brasil - 2020</h3>
+        <h3 class="ui inverted header">Curitiba, Paraná - Brasil 2020</h3>
         <a class="tiny ui youtube button" type="button" href="https://www.youtube.com/user/l32759">
           <i class="youtube icon"></i>
           YouTube
